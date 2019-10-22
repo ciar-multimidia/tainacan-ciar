796e79e7f9c50b256622c434e91aee7997dafd38
Ana Flavia Cador <aflaviah15@gmail.com>
2019-10-22 08:31:21 -0300

    finalizacao tema ciar tainacan

--- front-page.php
+++ front-page.php
@@ -13,7 +13,7 @@ echo '<section class="home-acervo">';
 
 			echo '<li>'; 
 				if($link) { echo '<a href="'.$link['url'].'">'; }
-					echo '<img src="'.$imagem['sizes']['medium'].'" alt="'.$imagem['alt'].'">';
+					echo '<img src="'.$imagem['sizes']['large'].'" alt="'.$imagem['alt'].'">';
 					if($tipo) { echo '<mark>'.$tipo.'</mark>'; }
 
 					echo '<hgroup>'; 
@@ -28,5 +28,58 @@ echo '<section class="home-acervo">';
 echo '</section>';
 }
 
+if( have_rows('imgslinha_2','option') ) { 
+echo '<section class="home-acervo">';
+	echo '<div class="wrap">';
+		echo '<div class="sub-wrap"><ul>';
+			while (have_rows('imgslinha_2','option')) : the_row();
+				$imagem = get_sub_field('imagem');
+				$link = get_sub_field('link');
+				$tipo = get_sub_field('tipo');
+				$area = get_sub_field('area');
 
- get_footer(); ?>
+			echo '<li>'; 
+				if($link) { echo '<a href="'.$link['url'].'">'; }
+					echo '<img src="'.$imagem['sizes']['large'].'" alt="'.$imagem['alt'].'">';
+					if($tipo) { echo '<mark>'.$tipo.'</mark>'; }
+
+					echo '<hgroup>'; 
+						echo '<h1>'.$imagem['title'].'</h1>';
+						if($area) {echo '<h2>'.$area.'</h2>';}
+					echo '</hgroup>';
+				if($link) { echo '</a>'; }
+			echo '</li>';
+			endwhile;
+		echo '</ul></div>';
+	echo '</div>';
+echo '</section>';
+}
+
+if( have_rows('imgslinha3','option') ) { 
+echo '<section class="home-acervo">';
+	echo '<div class="wrap">';
+		echo '<div class="sub-wrap"><ul>';
+			while (have_rows('imgslinha3','option')) : the_row();
+				$imagem = get_sub_field('imagem');
+				$link = get_sub_field('link');
+				$tipo = get_sub_field('tipo');
+				$area = get_sub_field('area');
+
+			echo '<li>'; 
+				if($link) { echo '<a href="'.$link['url'].'">'; }
+					echo '<img src="'.$imagem['sizes']['large'].'" alt="'.$imagem['alt'].'">';
+					if($tipo) { echo '<mark>'.$tipo.'</mark>'; }
+
+					echo '<hgroup>'; 
+						echo '<h1>'.$imagem['title'].'</h1>';
+						if($area) {echo '<h2>'.$area.'</h2>';}
+					echo '</hgroup>';
+				if($link) { echo '</a>'; }
+			echo '</li>';
+			endwhile;
+		echo '</ul></div>';
+	echo '</div>';
+echo '</section>';
+}
+
+get_footer(); ?>
--- inc/disclaimer.php
+++ inc/disclaimer.php
@@ -2,5 +2,5 @@
 echo '<div class="aviso-copyright"'; 
 	if (is_home() || is_front_page()) { echo ' id="aviso-popup"'; }
 echo '>'; 
-	echo '<p>As imagens dispostas aqui podem ser usadas e compartilhadas por terceiros, inclusive em sala de aula e pesquisas acadêmicas, desde que acompanhadas dos créditos de seus autores. A distribuição é gratuita e o uso comercial proibido.</p><button class="btn btn-outline-dark">Entendi</button>';
+	echo '<p>As imagens dispostas aqui podem ser usadas e compartilhadas por terceiros, inclusive em sala de aula e pesquisas acadêmicas, desde que acompanhadas dos créditos do Ciar UFG. A distribuição é gratuita e o uso comercial proibido.</p><button class="btn btn-outline-dark">Entendi</button>';
 echo '</div>';
